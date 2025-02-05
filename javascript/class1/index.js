cb29307803a33bf5d71f16a8b59ebc792bc025bf
nikhilnishadoo7 <rrnrrn007@gmail.com>
2025-02-05 11:22:04 +0530

    all

---
+++ javascript/class1/index.js
@@ -0,0 +1,14 @@
+
+
+let n = prompt("Enter a number: ");
+console.log(typeof n);
+let nw =Number(n);
+console.log(typeof nw);
+
+if(nw%2==0){
+    console.log(true);
+    
+}
+else{
+    console.log(false); 
+}
